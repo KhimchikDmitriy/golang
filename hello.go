adf9b97a65807f64bb8bba861a98930f3ab4c899
Химчик Дмитрий <95737507+KhimchikDmitriy@users.noreply.github.com>
2024-05-24 23:46:28 +0300

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+    var hello string = "Hello"
+    fmt.Println(hello)
+    var (
+        name string = "Den"
+        age int = 27
+    )
+    fmt.Println(name, age)
+    tom := "Tom"
+    fmt.Println(tom)
+    const (
+        a = 1
+        b
+        c
+        d = 3
+        f
+    )
+    fmt.Println(a,b,c,d,f)
+ }
---
+++ if_else.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+ 
+func main() {
+     
+	c := 8
+    b := 8
+    if c < b {
+        fmt.Println("c меньше b")
+    }else if c > b{
+        fmt.Println("c больше b")
+    } else{
+        fmt.Println("c равно b")
+    }
+
+	a := 8
+    switch(a) {
+        case 9: 
+            fmt.Println("a = 9")
+        case 8: 
+            fmt.Println("a = 8")
+        case 7: 
+            fmt.Println("a = 7")
+		default: 
+            fmt.Println("значение переменной a не определено")
+    }
+}
